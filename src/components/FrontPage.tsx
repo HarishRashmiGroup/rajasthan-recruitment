63fb1870f1b5aef36e5269c8260d4ec0eccb5145
HarishRashmiGroup <harish.kumar@rashmigroup.com>
2025-07-19 09:59:32 +0530

    build fix

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Image, Stack, Text } from "@chakra-ui/react";
+import { Image, Stack, Text } from "@chakra-ui/react";
 // import AnnouncementBar from "./AnnouncementBar";
 
 export const FrontPage = () => {
