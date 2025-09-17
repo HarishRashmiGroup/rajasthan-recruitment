885cf52b2a0d75600394a9a6b9e483749c138fdf
harishbisu <harishkb.ug20.cse@nitp.ac.in>
2025-09-17 07:08:06 +0530

    ad responsive width

--- src/components/ui/google-sidebar-ads.tsx
+++ src/components/ui/google-sidebar-ads.tsx
@@ -25,9 +25,10 @@ const AdSenseAd = () => {
   return (
     <ins
       className="adsbygoogle"
-      style={{ display: "inline-block", width: 200, height: 450 }}
+      style={{ display: "inline-block" }}
       data-ad-client="ca-pub-3383607348636418"
-      data-ad-slot="7891680227"
+      data-ad-slot="4740370254"
+      data-full-width-responsive="true"
     />
   );
 };
