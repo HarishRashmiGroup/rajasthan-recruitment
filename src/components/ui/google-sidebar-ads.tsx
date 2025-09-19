d91588cac91496e0a63a8acef61eaf9908271fdb
harishbisu <harishkb.ug20.cse@nitp.ac.in>
2025-09-19 11:44:44 +0530

    revert

--- src/components/ui/google-sidebar-ads.tsx
+++ src/components/ui/google-sidebar-ads.tsx
@@ -27,7 +27,7 @@ const AdSenseAd = () => {
       className="adsbygoogle"
       style={{ display: "block" }}
       data-ad-client="ca-pub-3383607348636418"
-      data-ad-slot="7891680227"
+      data-ad-slot="4740370254"
       data-ad-format="auto"
       data-full-width-responsive="true"
     />
