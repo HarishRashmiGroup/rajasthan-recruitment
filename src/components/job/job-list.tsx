d654cd8c62ab3ac2afe9e90f9ec3fa1ec5b72f7a
harishbisu <harishkb.ug20.cse@nitp.ac.in>
2025-09-26 08:03:11 +0530

    build fix

--- src/components/job/job-list.tsx
+++ src/components/job/job-list.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 import type { Job } from "../../types";
 import JobCard from "./job-card";
 import AdSenseAd from "../ui/google-sidebar-ads";
@@ -8,42 +8,42 @@ interface JobListProps {
   jobs: Job[];
 }
 
-const JobList: React.FC<JobListProps> = ({ jobs }) => {
-  const isMobile = useBreakpointValue({ base: true, lg: false });
+const JobList: React.FC<JobListProps> = ({ jobs }) => (
+  <Grid
+    gap={4}
+    templateColumns={{ base: "1fr", lg: "repeat(2, 1fr)" }}
+    width="full"
+  >
+    {jobs.map((job, idx) => (
+      <React.Fragment key={job.id}>
+        <GridItem>
+          <JobCard job={job} />
+        </GridItem>
 
-  return (
-    <Grid
-      gap={4}
-      templateColumns={{ base: "1fr", lg: "repeat(2, 1fr)" }}
-      width="full"
-    >
-      {jobs.map((job, idx) => (
-        <React.Fragment key={job.id}>
-          <GridItem>
-            <JobCard job={job} />
+        {(idx + 1) % 3 === 0 && (
+          <GridItem
+            colSpan={1}
+            display={{ base: "block", lg: "none" }}
+          >
+            <Box w="full">
+              <AdSenseAd />
+            </Box>
           </GridItem>
+        )}
 
-          {isMobile ? (
-            (idx + 1) % 3 === 0 && (
-              <GridItem colSpan={1}>
-                <Box w="full">
-                  <AdSenseAd />
-                </Box>
-              </GridItem>
-            )
-          ) : (
-            (idx + 1) % 2 === 0 && (
-              <GridItem colSpan={2}>
-                <Box w="full">
-                  <AdSenseAd />
-                </Box>
-              </GridItem>
-            )
-          )}
-        </React.Fragment>
-      ))}
-    </Grid>
-  );
-};
+        {(idx + 1) % 2 === 0 && (
+          <GridItem
+            colSpan={2}
+            display={{ base: "none", lg: "block" }} 
+          >
+            <Box w="full">
+              <AdSenseAd />
+            </Box>
+          </GridItem>
+        )}
+      </React.Fragment>
+    ))}
+  </Grid>
+);
 
 export default JobList;
