581afb58faf63e94cada1a30952e30f7afab95f1
harishbisu <harishkb.ug20.cse@nitp.ac.in>
2025-10-08 12:18:20 +0530

    google ads improve

--- src/components/ui/google-sidebar-ads.tsx
+++ src/components/ui/google-sidebar-ads.tsx
@@ -25,7 +25,7 @@ const AdSenseAd = () => {
   return (
     <ins
       className="adsbygoogle"
-      style={{ display: "inline-block", width: 200, height: "auto" }}
+      style={{ display: "inline-block", width: 200, height: 600 }}
       data-ad-client="ca-pub-3383607348636418"
       data-ad-slot="7891680227"
     />
